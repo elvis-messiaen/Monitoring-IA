750f2a48c004b2df57104576398cc3c1e2c9fedf
Elvis <elvism72@gmail.com>
2025-11-18 11:15:26 +0100

    Grafana & prometeus

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "API en ligne"}
+
+# Instrumentation Prometheus
+Instrumentator().instrument(app).expose(app)
